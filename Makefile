f2d013874fe5226dc158095de6e04ee150ecdb04
Бугианишвили <skulaxo98@mail.ru>
2022-10-21 00:21:51 +0600

    1day

---
+++ Makefile
@@ -0,0 +1,5 @@
+install:
+	npm install ci
+
+gendiff:
+	node bin/gendiff.js
---
+++ bin/gendiff.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander';
+
+const program = new Command();
+
+program
+    .description('Compares two configuration files and shows a difference.')
+    .version('0.0.1', '-v, --vers', 'output the current version')
+    
+    
+
+
+program.parse();
+
