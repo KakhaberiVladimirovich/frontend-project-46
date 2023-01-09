d028d4302ae2d2f4d3f4e298da45e45fd2c27b48
Бугианишвили <skulaxo98@mail.ru>
2023-01-09 14:30:04 +0600

    test

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,4 +1,3 @@
-import { test, expect } from '@jest/globals';
 import { fileURLToPath } from 'url';
 import { readFileSync } from 'fs';
 import { dirname, resolve } from 'path';
@@ -17,8 +16,8 @@ const json = readFile('json.txt');
 const format = ['json', 'yaml', 'yml'];
 
 test.each(format)('Check different file formats', (extension) => {
-  const fileName1 = `${process.cwd()}/__fixtures__/file1.${extension}`;
-  const fileName2 = `${process.cwd()}/__fixtures__/file2.${extension}`;
+  const fileName1 = readFile(`file1.${extension}`);
+  const fileName2 = readFile(`file2.${extension}`);
 
   expect(genDiff(fileName1, fileName2, 'stylish')).toEqual(stylish);
   expect(genDiff(fileName1, fileName2, 'plain')).toEqual(plain);
