510299816e2df0ec15b1264ad71993990a2b70f6
Бугианишвили <skulaxo98@mail.ru>
2023-01-09 14:45:56 +0600

    test

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -10,17 +10,17 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => resolve(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
-const stylish = readFile('stylish.txt');
-const plain = readFile('plain.txt');
-const json = readFile('json.txt');
-const format = ['json', 'yaml', 'yml'];
+const cases = [
+  ['file1.json', 'file2.json', 'json.txt', 'json'],
+  ['file1.yml', 'file2.yml', 'stylish.txt', 'stylish'],
+  ['file1.json', 'file2.json', 'stylish.txt', 'stylish'],
+  ['file1.json', 'file2.json', 'plain.txt', 'plain'],
+];
 
-test.each(format)('Check different file formats', (extension) => {
-  const fileName1 = `${process.cwd()}/__fixtures__/file1.${extension}`;
-  const fileName2 = `${process.cwd()}/__fixtures__/file2.${extension}`;
-
-  expect(genDiff(fileName1, fileName2, 'stylish')).toEqual(stylish);
-  expect(genDiff(fileName1, fileName2, 'plain')).toEqual(plain);
-  expect(genDiff(fileName1, fileName2, 'json')).toEqual(json);
-  expect(genDiff(fileName1, fileName2)).toEqual(stylish);
+test.each(cases)('Compare %s and %s to expect %s in "%s" style', (firstArg, secondArg, expectedResult, format) => {
+  const firstFile = getFixturePath(firstArg);
+  const secondFile = getFixturePath(secondArg);
+  const getResult = readFile(expectedResult);
+  const result = genDiff(firstFile, secondFile, format);
+  expect(result).toEqual(getResult);
 });
