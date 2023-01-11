fdae6738ebafdcd9f41f2f48fa87574e788d33f9
Бугианишвили <skulaxo98@mail.ru>
2023-01-11 21:19:12 +0600

    fix5

--- src/index.js
+++ src/index.js
@@ -8,13 +8,12 @@ const readFile = (filepath) => readFileSync(path.resolve(process.cwd(), filepath
 
 const extractFormat = (filename) => path.extname(filename).slice(1);
 
-const getData = (filepath) => parse(extractFormat(filepath));
+const getData = (filepath, content) => parse((extractFormat(filepath)), content);
 
 const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
-  const fileContent1 = readFile(filepath1);
-  const fileContent2 = readFile(filepath2);
-  const data1 = getData(fileContent1);
-  const data2 = getData(fileContent2);
+  const data1 = getData(filepath1, readFile(filepath1));
+  const data2 = getData(filepath2, readFile(filepath2));
+
   const innerTree = buildTree(data1, data2);
 
   return format(innerTree, formatName);
